7d21cd8f8219afac5f197ce0ef6f0900b0bf731d
olgakos <ok2304@yandex.ru>
2026-04-01 23:21:51 +0300

    commit1

---
+++ src/main/java/guru/qa/lesson/First.java
@@ -0,0 +1,15 @@
+package guru.qa.lesson;
+
+public class First {
+
+    public static void main(String[] args) {
+
+        //Целочисленные типы данных
+        byte aByte = 0; // 8 бит -128 - 127
+        short aShort = 0; // 16 бит
+        int aInt = 0; // 32 бита
+        long aLong = 0L; // 64 бита
+
+
+    }
+}
